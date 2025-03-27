bd050224913d194790db0bbe0103ae14356e1cf6
Jakirul458 <rimon458jacky@gmail.com>
2025-03-27 23:08:31 +0530

    change backend url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
     // baseURL: import.meta.env.VITE_API_URL,
-    baseURL:'https://gfst-backend.onrender.com',
+    baseURL:'https://gfst-server.onrender.com',
     timeout: 3 * 60 * 1000, 
     // withCredentials: true,
     headers: {
